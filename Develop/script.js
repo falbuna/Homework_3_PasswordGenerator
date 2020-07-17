ec1a28525386e9234333207a1eef77e74f19762c
Francis Albuna <falbuna1@gmail.com>
2020-07-17 16:40:29 -0700

    Got a 4 letter lowercase password to generate when button is pressed.

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+
+  var lowcletters = ('abcdefghijklmnopqrstuvwxyz');
+  var length = 4;
+  var randomlet = '';
+    for (i=0; i < length; i++){
+      var rletter = Math.floor(Math.random() * lowcletters.length);
+      randomlet += lowcletters.substring(rletter, rletter+1);
+    }
+    return randomlet;
+}
+
 // Write password to the #password input
-function writePassword() {
+function writePassword() { 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
