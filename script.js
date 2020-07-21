7542db40f8c7e06d60463934dda5dfd7c18fcd9c
Francis Albuna <falbuna1@gmail.com>
2020-07-20 19:47:41 -0700

    Edited the comments in the script.js file.

--- script.js
+++ script.js
@@ -38,38 +38,38 @@ function generatePassword() {
   const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
   // This will split the string of numbers into an array of substrings.
   const numeric = '0123456789'.split('');
-  // This will spint the string of special characters into an array of substrings.
+  // This will split the string of special characters into an array of substrings.
   const special = '\xa0!\"#$%&\'\(\)*+\,.-/;:\<\=\>\\?@\]\[^_`\}\{|~'.split('');
 
-  // This is a placeholder for the array that will contain all the arrays that are confirmed with the prompts.
+  // This is a placeholder for the array that will contain all the strings that are confirmed with the prompts.
   var combinedarray = [];
 
   // This prompt will ask if they want to use lowercase letters.
   var lc = confirm('Do you want to use lowercase letters?');
       if (lc == true){
-        // If the statement is true, add the lowercase array. If false, do not add the lowercase array.
+        // If the user pressed "OK" on the prompt, add the lowercase strings into the array. If the user pressed canel, do not add the lowercase strings.
         combinedarray = combinedarray.concat(lowercase);
       }
   // This prompt will ask if they want to use uppercase letters.
   var uc = confirm('Do you want to use uppercase letters?');
       if (uc == true){
-        // If the statement is true, add the uppercase array. If false, do not add the uppercase array.
+        // If the user pressed "OK" on the prompt, add the uppercase strings into the array. If the user pressed cancel, do not add the uppercase strings.
         combinedarray = combinedarray.concat(uppercase);
       }
   // This prompt will ask if they want to use numerical values.
   var nm = confirm('Do you want to use numbers?');
             if (nm == true){
-        // If the statement is true, add the numerical characters array. If false, do not add the numerical characters array.
+        // If the user pressed "OK" on the prompt, add the numerical strings into the array. If the user pressed cancel, do not add the numerical strings.
         combinedarray = combinedarray.concat(numeric);
       }
   // This prompt will ask if they want to use special characters.
   var sp = confirm('Do you want to use special characters?');
             if (sp == true){
-        // If the statement is true, ad the special characters array. If false, do not add the special characters array.
+        // If the user pressed "OK" on the prompt, add the string of special characters into the array. If user pressed cancel, do not add the special strings.
         combinedarray = combinedarray.concat(special);
             }
       
-    // This will turn the combined array placeholder into a string with all the characters that the user desired through the previous prompts.
+    // This will convert the elements of the combined array placeholder into a string.
     var password = combinedarray.join('');
 
     // This will generate the random password with the length and the characters allowed through the previous prompts.
